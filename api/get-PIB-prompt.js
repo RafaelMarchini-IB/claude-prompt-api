0e9b266141d45ba58f36ea2ecd63e271bc1946d3
RafaelMarchini-IB <rafael@ignatious.com>
2026-05-07 11:44:50 -0300

    Update get-PIB-prompt.js

--- api/get-PIB-prompt.js
+++ api/get-PIB-prompt.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
   try {
     const url =
-      "https://docs.google.com/document/d/1xxIoDlgJbj4Yi8yz19pjlzKJ6LqeMZP0Z7Yqxex7ItU/export?format=txt";
+      "https://docs.google.com/document/d/1xxIoDlgJbj4Yi8yzl9pjlzKJ6LqeMZP0Z7Yqxex7ItU/export?format=txt";
 
     const response = await fetch(url);
     const text = await response.text();
